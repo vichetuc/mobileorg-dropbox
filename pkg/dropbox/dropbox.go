6a5f0fc5d1af6bfb96527621d700e4d0546eeb2b
Lenilson J. Dias <lenilson@gmail.com>
2014-09-01 18:30:43 -0300

    Uploading Files

---
+++ pkg/dropbox/dropbox.go
@@ -0,0 +1,46 @@
+// Copyright 2014 Lenilson Jose Dias. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// Package dropbox implements the interface with Dropbox API.
+//
+// This package uses the library: [https://github.com/scottferg/Dropbox-Go]
+
+package dropbox
+
+import (
+	"io/ioutil"
+	"path"
+
+	"github.com/scottferg/Dropbox-Go/dropbox"
+)
+
+// Get URI to remote Dropbox file.
+func getUri(filename string, path string) dropbox.Uri {
+	uriPath := dropbox.Uri{
+		Root: dropbox.RootDropbox,
+		Path: path + filename,
+	}
+	return uriPath
+}
+
+// Upload a file using dropbox connection
+func UploadFile(ds dropbox.Session, local string, remote string) (bool, error) {
+	fname := path.Base(local)
+	uriPath := getUri(fname, remote)
+
+	if file, err := ioutil.ReadFile(local); err != nil {
+		return false, err
+	} else {
+		_, err := dropbox.UploadFile(ds, file, uriPath, nil)
+
+		if err != nil {
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func DownloadFile(dir string) (bool, error) {
+	return true, nil
+}
